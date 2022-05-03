5adc621293a78a0c33ef94b6ce5e134d8b5e16af
Quang Minh <54538472+quangmnh@users.noreply.github.com>
2022-05-03 22:38:57 +0700

    Update ui_main.py

--- views/ui_main.py
+++ views/ui_main.py
@@ -958,7 +958,6 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
