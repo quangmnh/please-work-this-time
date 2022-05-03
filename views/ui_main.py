110e0fdb5a1a5f078f20143d6948715b5cc73d9c
Quang Minh <54538472+quangmnh@users.noreply.github.com>
2022-05-03 22:29:50 +0700

    Update ui_main.py

--- views/ui_main.py
+++ views/ui_main.py
@@ -959,8 +959,8 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
+    MainWindow.showMaximized()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
-    MainWindow.showMaximized()
     sys.exit(app.exec_())
